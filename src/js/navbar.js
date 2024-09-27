0975c45b007ab0819b0d837895a6aef608f42057
santi-design <oviedos412@gmail.com>
2024-09-26 22:48:33 -0300

    Add about section

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,5 +1,4 @@
 const navbarToggler = document.getElementById("my-navbar-toggler");
-console.log(navbarToggler);
 
 navbarToggler.addEventListener("click", () => {
   document.querySelector(".navbar-collapse").classList.toggle("show-navbar");
