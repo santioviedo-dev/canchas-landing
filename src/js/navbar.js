a779eb2123e4ee1052f179a4259a37427a7cf69b
santi-design <oviedos412@gmail.com>
2024-09-28 22:24:06 -0300

    Add contact section

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,3 +3,17 @@ const navbarToggler = document.getElementById("my-navbar-toggler");
 navbarToggler.addEventListener("click", () => {
   document.querySelector(".navbar-collapse").classList.toggle("show-navbar");
 });
+
+// Obtener el ancho del viewport y el ancho total del documento
+
+document.addEventListener("DOMContentLoaded", () => {
+  const viewportWidth = window.innerWidth;
+  const documentWidth = document.documentElement.scrollWidth;
+
+  const scrollWidth = documentWidth - viewportWidth;
+
+  const img = document.querySelector(".contact-img");
+  console.log(scrollWidth);
+
+  img.style.cssText += `width: calc(100vw - ${-scrollWidth}px) !important;`;
+});
